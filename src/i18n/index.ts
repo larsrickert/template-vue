76dc17b41576042c67ae738e97a359d0a0cbaac1
Lars Rickert <67898185+larsrickert@users.noreply.github.com>
2022-12-19 00:16:34 +0100

    chore: add type inference for translation keys

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,16 @@
 import messages from "@intlify/unplugin-vue-i18n/messages";
 import { createI18n } from "vue-i18n";
 import datetimeFormats from "./datetime-formats";
+import type en from "./locales/en.json";
 import numberFormats from "./number-formats";
+
+// add type inference/autocompletion for translation keys
+declare module "vue-i18n" {
+  type Message = typeof en;
+  export interface DefineLocaleMessage extends Message {
+    [key: string]: string;
+  }
+}
 
 const i18n = createI18n({
   locale: "en",
